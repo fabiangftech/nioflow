caf507c215c210c4a086a57878a2b907790ac967
fabiangftech <fabian.gf.tech@gmail.com>
2026-07-10 16:27:10 -0400

    chore: add module springboot

---
+++ examples/springboot-with-nioflow/src/main/java/dev/nioflow/springbootwithnioflow/SpringbootWithNioflowApplication.java
@@ -0,0 +1,13 @@
+package dev.nioflow.springbootwithnioflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWithNioflowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootWithNioflowApplication.class, args);
+    }
+
+}
---
+++ examples/springboot-with-nioflow/src/test/java/dev/nioflow/springbootwithnioflow/SpringbootWithNioflowApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.nioflow.springbootwithnioflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootWithNioflowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
